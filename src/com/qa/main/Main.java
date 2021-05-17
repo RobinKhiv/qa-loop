fc66910ce62d750ff7393423e87e852d37b79727
Robin Khiv <robinkhiv@gmail.com>
2021-05-17 13:20:10 -0700

    rk:finished Number exercise 2

--- src/com/qa/main/Main.java
+++ src/com/qa/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a = Number.getString(20);
+		String a = Number.getString(26);
 		System.out.println(a);
 	}
 
