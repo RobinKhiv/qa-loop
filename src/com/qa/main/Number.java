5a43bc67a2bd38bccb4750d17629d77522e8fdd8
Robin Khiv <robinkhiv@gmail.com>
2021-05-17 12:30:59 -0700

    rk:added Number class

---
+++ src/com/qa/main/Number.java
@@ -0,0 +1,10 @@
+package com.qa.main;
+
+public class Number {
+	// Create a method that takes a number 10-99, 
+	// and adds the two digits together for example 74 = 7 + 4 = 11.
+	
+	public static int add(int i, int j) {
+		return i+j;
+	}
+}
