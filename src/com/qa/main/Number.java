3b53d138a45bf6ed6f3bf55373d59b1a485a3ccf
Robin Khiv <robinkhiv@gmail.com>
2021-05-18 07:19:38 -0700

    rk:added more excercises

--- src/com/qa/main/Number.java
+++ src/com/qa/main/Number.java
@@ -32,4 +32,18 @@ public class Number {
 		
 		return result;
 	}
+	//Create a second method that when given the number 1-99 returns a String representation of this number, for example 1 = one, 11 = eleven, 21 = twenty-one.
+			//Expand on the method you wrote in Numbers 2 to allow the input 1-999.
+			//Expand the method you wrote in Numbers 2 to allow the input 1-9999
+
+	//Numbers Revisited		
+	//Use a for()-loop to print the numbers 1-100 in words.
+	//For example; 1 = one, 100 = one hundred.
+			
+	//Given a cost and an amount, work out the change given in specific coinage.
+	//For example; the cost is £4.58 and the customer pays with a £20 note so as change they receive:
+	//1 £10 note
+	//1 £5 note
+	//2 20p's
+	//1 2p
 }
